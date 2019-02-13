212d8ad4379e62d9694f659aa72e79119faed343
Alireza Mosajjal <mosajjal@gmail.com>
2019-02-13 09:12:49 +0330

    autopush script test

---
+++ autopush.py
@@ -0,0 +1,19 @@
+import docker
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+versions = ["2.7", "3.5", "3.6"]
+
+if __name__ == "__main__":
+    client = docker.from_env()
+
+    for item in versions:
+        print(f"pypy {item} will be built")
+        print("building the new image")
+        
+        img = client.images.build(path=current_dir+f"/{item}")
+        print("pypy {item} image built successfully, tagging the image")
+        img[0].tag(f"mosajjal/pypy:{item}")
+
+        print("pushing the repository to docker hub")
+        client.images.push(repository="mosajjal/pypy", tag=f"{item}")
