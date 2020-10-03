ba318e3d01f4d72f7b235d38e99a0568895f6ce6
Alireza Mosajjal <mosajjal@gmail.com>
2020-10-03 13:12:32 +1300

    removed 3.5 and 2.7, added 3.7 and version to latest

--- autopush.py
+++ autopush.py
@@ -2,7 +2,9 @@ import docker
 import os
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-versions = ["2.7", "3.5", "3.6"]
+versions = ["3.6", "3.7"]
+latest_version = "3.7"
+repo = "mosajjal/pypy"
 
 if __name__ == "__main__":
     client = docker.from_env()
@@ -10,10 +12,14 @@ if __name__ == "__main__":
     for item in versions:
         print(f"pypy {item} will be built")
         print("building the new image")
-        
+
         img = client.images.build(path=current_dir+f"/{item}")
-        print("pypy f{item} image built successfully, tagging the image")
-        img[0].tag(f"mosajjal/pypy:{item}")
+        print(f"pypy f{item} image built successfully, tagging the image")
+        img[0].tag(f"{repo}:{item}")
 
         print("pushing the repository to docker hub")
-        client.images.push(repository="mosajjal/pypy", tag=f"{item}")
+        client.images.push(repository=repo, tag=f"{item}")
+
+    latest_image = client.images.get(f"{repo}:{latest_version}")
+    latest_image.tag(f"{repo}:latest")
+    client.images.push(repository=repo, tag="latest")
